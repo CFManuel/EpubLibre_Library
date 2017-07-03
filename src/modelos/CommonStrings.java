ba71ddde7573d80a304d6576225625330954e5f0
ladaga <davidmtn@gmail.com>
2017-07-03 23:45:12 +0200

    Version 0.5.

--- src/modelos/CommonStrings.java
+++ src/modelos/CommonStrings.java
@@ -22,6 +22,7 @@ package modelos;
  * Created by david on 03/07/2017.
  */
 public interface CommonStrings {
+    //Version del softwar 0.5.
     String VERSION = "v0.5.";
     String LAST_UPDATE = "LAST_UPDATE";
     String CSV_URL = "https://epublibre.org/rssweb/csv";
