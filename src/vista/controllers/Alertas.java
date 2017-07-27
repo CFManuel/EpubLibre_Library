ab64a7db6f991d6e197f2366c427a05b7a668315
ladaga <davidmtn@gmail.com>
2017-07-27 13:21:46 +0200

    Refactored code alerts.

--- src/vista/controllers/Alertas.java
+++ src/vista/controllers/Alertas.java
@@ -37,6 +37,9 @@ import java.net.URISyntaxException;
 import java.util.Optional;
 
 public class Alertas implements CommonStrings {
+    private static final String ICONO_ALERTAS = "vista/resources/images/icono_alertas.png";
+    private static final String SIMBOLO_EPL = "vista/resources/images/EPL_Portadas_NEGRO.png";
+
     private Alertas() {
     }
 
@@ -45,7 +48,7 @@ public class Alertas implements CommonStrings {
      */
     public static void alertUpdateOK() {
         Alert ok = new Alert(Alert.AlertType.INFORMATION);
-        ((Stage) ok.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
+        ((Stage) ok.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
         ok.setHeaderText("Se han cargado los libros con éxito.");
         ok.setContentText("Actualización finalizada.");
         ok.show();
@@ -53,14 +56,13 @@ public class Alertas implements CommonStrings {
 
     public static void alertNewAppUpdate() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
-        alert.setGraphic(new ImageView(new Image("vista/resources/images/icono_alertas.png")));
+        ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
+        alert.setGraphic(new ImageView(new Image(ICONO_ALERTAS)));
         alert.setHeaderText("Hay una nueva versión disponible. (" + Main.getConfiguracion().get(VERSION_CHECK) + ")");
         alert.setContentText("¿Desea abrir el navegador para ver la nueva versión?");
         ButtonType epl = new ButtonType("ePL");
         ButtonType dropbox = new ButtonType("Dropbox");
-        ButtonType cancel = new ButtonType("Cancelar");
-        alert.getButtonTypes().setAll(epl, dropbox, cancel);
+        alert.getButtonTypes().setAll(epl, dropbox, ButtonType.CLOSE);
 
         Label label = new javafx.scene.control.Label("Cambios en la última versión:");
         TextArea textArea = new TextArea(Main.getConfiguracion().get(NEWS));
@@ -98,7 +100,7 @@ public class Alertas implements CommonStrings {
      */
     public static void alertUpdateFail() {
         Alert fail = new Alert(Alert.AlertType.ERROR);
-        ((Stage) fail.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
+        ((Stage) fail.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
         fail.setHeaderText("No se han podido actualizar los libros.");
         fail.setContentText("Me avergüenza decirlo, pero algo no ha salido bien...");
         fail.show();
@@ -112,12 +114,12 @@ public class Alertas implements CommonStrings {
      */
     public static void alertDBInformation(String fecha, int libros) {
         Alert info = new Alert(Alert.AlertType.INFORMATION);
-        ((Stage) info.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
-        info.setGraphic(new ImageView(new Image("vista/resources/images/icono_alertas.png")));
+        ((Stage) info.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
+        info.setGraphic(new ImageView(new Image(ICONO_ALERTAS)));
         info.setHeaderText("Información de actualización de la base de datos:");
         info.setContentText(
                 String.format(
-                        "La base de datos se actualizo por ultima vez en %s. %n"
+                        "La base de datos se actualizo por ultima vez el %s. %n"
                                 + "Actualmente contiene %d libros.",
                         fecha, libros));
         info.show();
@@ -128,8 +130,8 @@ public class Alertas implements CommonStrings {
      */
     public static void aplicationInfo() {
         Alert info = new Alert(Alert.AlertType.INFORMATION);
-        ((Stage) info.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
-        info.setGraphic(new ImageView(new Image("vista/resources/images/icono_alertas.png")));
+        ((Stage) info.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
+        info.setGraphic(new ImageView(new Image(ICONO_ALERTAS)));
         info.setTitle("EpubLibrary " + VERSION);
         info.setHeaderText("Created by and for ePubLibre.");
         info.setContentText(
@@ -142,7 +144,7 @@ public class Alertas implements CommonStrings {
      */
     public static void alertDBError() {
         Alert error = new Alert(Alert.AlertType.ERROR);
-        ((Stage) error.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
+        ((Stage) error.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
         error.setHeaderText("Ha habido un error al realizar consultar los datos");
         error.setContentText("Me avergüenza decirlo, pero algo no ha salido bien...");
         error.show();
@@ -155,7 +157,7 @@ public class Alertas implements CommonStrings {
      */
     public static Optional<ButtonType> updateConfirmation() {
         Alert sure = new Alert(Alert.AlertType.CONFIRMATION);
-        ((Stage) sure.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
+        ((Stage) sure.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
         sure.setHeaderText(
                 "La actualización puede tardar unos minutos, dependiendo de tu conexión a internet.");
         sure.setContentText("¿Está seguro que desea continuar?");
@@ -169,7 +171,7 @@ public class Alertas implements CommonStrings {
      */
     public static Optional<ButtonType> panicButton() {
         Alert sure = new Alert(Alert.AlertType.CONFIRMATION);
-        ((Stage) sure.getDialogPane().getScene().getWindow()).getIcons().add(new Image("vista/resources/images/EPL_Portadas_NEGRO.png"));
+        ((Stage) sure.getDialogPane().getScene().getWindow()).getIcons().add(new Image(SIMBOLO_EPL));
         sure.setHeaderText("¿Desea restablecer los valores por defecto?");
         sure.setContentText("Deberá reiniciar la aplicación para que los cambios tengan efecto.");
         return sure.showAndWait();
