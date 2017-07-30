3c78d8e335dc335fededf7a527ec1b40458a069c
ladaga <davidmtn@gmail.com>
2017-07-30 16:21:01 +0200

    Fixed paragraph detection.

--- src/modelos/CommonStrings.java
+++ src/modelos/CommonStrings.java
@@ -42,5 +42,5 @@ public interface CommonStrings {
     String PATTERN_I = "[îíìï]";
     String PATTERN_O = "[ôöóò]";
     String PATTERN_U = "[úùûü]";
-    String PATTERN_FOR_PARAGRAPH = "([a-zA-Z][\\.!?])([A-Z])";
+    String PATTERN_FOR_PARAGRAPH = "(.[\\.!?])([A-Z«¿¡—])";
 }
